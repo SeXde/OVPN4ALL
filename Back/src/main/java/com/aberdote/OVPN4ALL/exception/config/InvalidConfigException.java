cdab5d2a3d5975dd50c9cf2438377cd29d6eae2d
Álvaro Berdote <avberdote@hotmail.com>
2022-10-20 22:41:21 +0200

    Add custom exceptions

---
+++ Back/src/main/java/com/aberdote/OVPN4ALL/exception/config/InvalidConfigException.java
@@ -0,0 +1,9 @@
+package com.aberdote.OVPN4ALL.exception.config;
+
+public class InvalidConfigException extends Exception{
+
+    public InvalidConfigException(String message) {
+        super(message);
+    }
+
+}
---
+++ Back/src/main/java/com/aberdote/OVPN4ALL/exception/user/InvalidUserException.java
@@ -0,0 +1,9 @@
+package com.aberdote.OVPN4ALL.exception.user;
+
+public class InvalidUserException extends Exception{
+
+    public InvalidUserException(String message) {
+        super(message);
+    }
+
+}
