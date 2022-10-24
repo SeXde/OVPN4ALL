f6b62b64a0229e51de603fa196477c5b0493fe7a
Álvaro Berdote <avberdote@hotmail.com>
2022-10-24 08:30:18 +0200

    Add cancel button

--- Front/src/routes/sign-up/+page.svelte
+++ Front/src/routes/sign-up/+page.svelte
@@ -76,7 +76,7 @@
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
 			  </svg>
-			  <h2 class="text-center text-2xl"> Register form</h2>
+			  <h2 class="text-center text-2xl">New user</h2>
 		</div>
         <div class="relative my-3">
             <input required bind:value={username} type="text" id="username" class="block px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 rounded-lg border-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
@@ -100,19 +100,24 @@
 		{#if formError}
 			<ErrorMessage title={"Form error"} body={formError} />
 		{/if}
-        {#if validData}
-            <button type="submit" class="my-3 mx-auto mt-5 w-36 py-2 flex justify-center text-light rounded-lg border-2 border-light hover:text-primary hover:border-primary disabled:border-stone-500 disabled:text-stone-500 font-semibold transition-colors">
-                {#if isLoading}
-                    <div class="border border-t-4 border-b-2 border-primary rounded-full  animate-spin">
-                        <div class="p-2"></div>
-                    </div>
-	            {:else}
-					Register user
-	            {/if}
-            </button>
-        {:else}
-            <button disabled type="submit" class="my-3 disabled:opacity-25 mx-auto mt-5 w-36 py-2 flex justify-center text-light rounded-lg border-2 border-light hover:text-primary hover:border-primary disabled:border-stone-500 disabled:text-stone-500 font-semibold transition-colors">Register user</button>
-        {/if}
+		<div class="flex flex-row">
+			{#if validData}
+				<button type="submit" class="my-3 mx-auto mt-5 w-36 py-2 flex justify-center text-light rounded-lg border-2 border-light hover:text-primary hover:border-primary disabled:border-stone-500 disabled:text-stone-500 font-semibold transition-colors">
+					{#if isLoading}
+						<div class="border border-t-4 border-b-2 border-primary rounded-full  animate-spin">
+							<div class="p-2"></div>
+						</div>
+					{:else}
+						Add
+					{/if}
+				</button>
+			{:else}
+				<button disabled type="submit" class="my-3 disabled:opacity-25 mx-auto mt-5 w-36 py-2 flex justify-center text-light rounded-lg border-2 border-light hover:text-primary hover:border-primary disabled:border-stone-500 disabled:text-stone-500 font-semibold transition-colors">Add</button>
+			{/if}
+				<a class="ml-3 my-3 mx-auto mt-5 w-36 py-2 flex justify-center text-light rounded-lg border-2 border-light hover:text-primary hover:border-primary disabled:border-stone-500 disabled:text-stone-500 font-semibold transition-colors" href="/users">
+					Cancel
+				</a>
+		</div>
         {#if postError}
             <ErrorMessage title={"Server error."} body={postError} />
         {/if}
