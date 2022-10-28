f5acf8decf466ada5b5a52694b32bdf324932f57
Alvaro Berdote <avberdote@hotmail.com>
2022-10-28 18:54:59 +0200

    Update application.yml and restrict users name

---
+++ Back/src/main/java/com/aberdote/OVPN4ALL/common/constanst/UserReservedConstants.java
@@ -0,0 +1,10 @@
+package com.aberdote.OVPN4ALL.common.constanst;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserReservedConstants {
+
+    public final static Set<String> USER_RESERVED = new HashSet<>(Set.of("ca, ovpn4all-server"));
+
+}
