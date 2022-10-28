63feb92e0dd588c75501a1ae4728bf5c65a13d77
Álvaro Berdote <avberdote@hotmail.com>
2022-10-28 19:32:05 +0200

    Change addUser script call

--- Back/src/main/java/com/aberdote/OVPN4ALL/common/constanst/ScriptsConstants.java
+++
@@ -1,8 +0,0 @@
-package com.aberdote.OVPN4ALL.common.constanst;
-
-public class ScriptsConstants {
-
-    public final static String APP_PATH = "/home/sexde/OVPN4ALL/Scripts/";
-    public final static String CREATE_USER_SH = "create_user.sh";
-
-}
