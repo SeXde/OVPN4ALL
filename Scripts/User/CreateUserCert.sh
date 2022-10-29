885b32d7dbb66b38075ba7e38eaaf0744d71f1ac
Alvaro Berdote <avberdote@hotmail.com>
2022-10-29 10:22:20 +0200

    decode passwords

--- Scripts/User/CreateUserCert.sh
+++ Scripts/User/CreateUserCert.sh
@@ -23,12 +23,14 @@ WD_DIR="$1"
 USERNAME="$3"
 PASSWORD="$4"
 
+
 function log() {
     printf "%s ...\n" "$1" &>> "$WD_DIR/$LOG_FILE"
     bash -c "$2" &>> "$WD_DIR/$LOG_FILE" ||  { printf "%s" "$3" &>> "$WD_DIR/$LOG_FILE"; exit 1; }
     
 }
 
-log "Creating user certificate" "cd $WD_DIR/Install/EasyRSA ; echo -en '$USERNAME\n' | ./easyrsa --passout=pass:$PASSWORD gen-req $USERNAME" "Cannot gen user cert"
+printf "%s ...\n" "Creating user certificate" &>> "$WD_DIR/$LOG_FILE"
+bash -c "cd $WD_DIR/Install/EasyRSA ; echo -en '$USERNAME\n' | ./easyrsa --passout=pass:'$(echo "$PASSWORD" | xxd -r -p)' gen-req $USERNAME" &>> "$WD_DIR/$LOG_FILE" ||  { printf "%s" "Cannot gen user cert" &>> "$WD_DIR/$LOG_FILE"; exit 1; }
 log "Signing user certificate" "cd $WD_DIR/Install/EasyRSA ; echo -en 'yes\n' | ./easyrsa sign-req client $USERNAME" "Cannot sign user cert"
 exit 0
